56f1862fb75e87c6c4f3b872d8cd1d2690ad60cc
Lucas Ramos <lucasramos.lr@gmail.com>
2022-09-02 22:19:24 -0300

    Adiciona a solução do exercício

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("O que é, o que é...");
+
+Thread.Sleep(5000);
+
+Console.WriteLine("que o nadador faz para bater o recorde?");
+
+Thread.Sleep(5000);
+
+Console.WriteLine("Nada.");
